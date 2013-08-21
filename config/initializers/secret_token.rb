e54a92918ea7b46f0d10c69e797ed221675c0a75
Michael Sands <michael.sands@generic.gen.nz>
2013-08-21 22:32:53 +1200

    raw new rails app for motw

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonsterOfTheWeek::Application.config.secret_key_base = 'd377eec534de60f216a29dfc97de51b1220d038320d639e427bf4897baa598ec7fe161cd6a04e279b7d5531a5684e985a89e09e32249cf49787baff15943f03e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MonsterOfTheWeek::Application.config.session_store :cookie_store, key: '_monster_of_the_week_session'
