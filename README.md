23e923ec2bf8a2ceeb14f53419baa0384a86c967
Michael Sands <michael.sands@generic.gen.nz>
2013-08-27 10:06:19 +1200

    add some basic layout, home page text, and some basic user functionality

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 monster_of_the_week
 ===================
 
-Monster of the Week webapp
+Monster of the Week webapp: 
+
+Plans:
+1. Users can sign in and add their monsters and other threats to a database
+2. Anyone can browse the entries
+3. Users can make a character and generate a filled-in version of the PDF.
+4. Include everything from existing website so this can replace it.
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ MonsterOfTheWeek::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root "home_page#home"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
