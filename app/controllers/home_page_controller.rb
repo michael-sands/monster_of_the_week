718cac01b8f1365955f14405ffa1bcc39b7d7cee
Michael Sands <michael.sands@generic.gen.nz>
2013-08-29 15:59:49 +1200

    tweak the layout so it looks nicer add header and footer add contact page begin support for user signup and login add custom webfonts, nice to see how that works add basic routes to config (including some pages that aren't there yet)

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,5 +1,18 @@
 class HomePageController < ApplicationController
 
   def home
+    # nothing special to do
   end
+  
+  def about
+    # nothing special to do
+  end
+  
+  def contact
+    # nothing special to do
+  end
+  
+  def news
+  end
+  
 end
