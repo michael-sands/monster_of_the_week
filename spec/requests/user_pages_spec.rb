abfc1b186c3261806dfd8dc3dd27b789fa569aaf
Michael Sands <michael.sands@generic.gen.nz>
2013-09-04 11:55:14 +1200

    final two user update tests now passing.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -69,9 +69,8 @@ describe "User pages" do
     end
 
     describe "page" do
-      it { should have_content("Update your profile") }
-      it { should have_title("Edit user") }
-      #it { should have_link('change', href: 'http://gravatar.com/emails') }
+      it { should have_content("Update your details") }
+      it { should have_title("Update User") }
     end
 
     describe "with invalid information" do
