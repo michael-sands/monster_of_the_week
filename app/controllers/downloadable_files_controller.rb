836331ee6848a4eab65d8eca1c4c189f5438f5b9
Michael Sands <michael.sands@generic.gen.nz>
2013-11-06 15:57:57 +1300

    fixed a couple of permissions issues in news articles when not signed in

--- app/controllers/downloadable_files_controller.rb
+++ app/controllers/downloadable_files_controller.rb
@@ -1,10 +1,21 @@
 class DownloadableFilesController < ApplicationController
   
-  def downloads
+  def new
+    if !current_user.admin_user
+      redirect_to downloadable_files_path
+    end
+    @file = DownloadableFile.new
   end
   
-  def new
-    @file = DownloadableFile.new
+  def create
+    if !current_user.admin_user
+      redirect_to root_url
+    else 
+      if @file.save 
+        flash[:success] = "File uploaded created"
+        redirect_to files_path
+      end   
+    end
   end
   
   def edit
@@ -16,5 +27,14 @@ class DownloadableFilesController < ApplicationController
   def show
     @file = DownloadableFile.find(params[:id])
   end
+  
+  def index
+    @files = DownloadableFile.paginate(page: params[:page], per_page: 20)
+  end
 
+private
+  
+    def downloadable_file_params
+      params.require(:downloadable_file).permit(:name, :display_name, :the_file)
+    end
 end
